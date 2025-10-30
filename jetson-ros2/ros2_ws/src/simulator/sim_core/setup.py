ae702b8256feffd2f5a1ea80404311e6cc30c15d
Bryan Xu <bryanx@cs.washington.edu>
2025-10-30 00:52:29 -0700

    Merged vis and core pkg to core

--- jetson-ros2/ros2_ws/src/simulator/sim_core/setup.py
+++ jetson-ros2/ros2_ws/src/simulator/sim_core/setup.py
@@ -5,24 +5,44 @@ package_name = 'sim_core'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=['sim_core'],
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/sim_core']),
-        ('share/sim_core', ['package.xml', 'README.md']),
-        ('share/sim_core/launch', ['launch/sim_launch.py']),
-        ('share/sim_core/launch', ['launch/sim_rviz.launch.py'])
+    packages=[
+        'sim_core',       # your original core stuff
+        'sim_core.viz',   # <- this will be the old sim_viz code (see note below)
     ],
-    install_requires=['setuptools', 'PyYAML'],
+    data_files=[
+        # ament index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+
+        # package.xml
+        ('share/' + package_name, ['package.xml', 'README.md']),
+
+        # launch files (from BOTH old packages)
+        ('share/' + package_name + '/launch', [
+            'launch/sim_launch.py',       # from old sim_core
+            'launch/sim_rviz.launch.py',  # from old sim_core (you can delete later)
+            'launch/viz.launch.py',       # from old sim_viz
+        ]),
+    ],
+    install_requires=[
+        'setuptools',
+        'PyYAML',
+    ],
+    zip_safe=True,
     maintainer='bryanx',
     maintainer_email='bryanx@cs.washington.edu',
-    description='Minimal simulator core for driverless FSAE (ROS2 Jazzy).',
-    license='MIT',
+    description='Minimal simulator core for driverless FSAE (ROS 2 Jazzy) with viz helpers.',
+    license='MIT AND BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # from old sim_core
             'sim_loop = sim_core.sim_loop:main',
             'mimic_perception = sim_core.mimic_perception:main',
-            'vehicle_dynamics = sim_core.vehicle_dynamics:main'
+            'vehicle_dynamics = sim_core.vehicle_dynamics:main',
+
+            # from old sim_viz
+            'publish_cone_map = sim_core.viz.publish_cone_map:main',
+            'pose_to_tf = sim_core.viz.pose_to_tf:main',
         ],
     },
 )
